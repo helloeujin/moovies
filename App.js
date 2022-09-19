2b2664793497508d341879149d53facf50daa10c
Youjin <meggonagul@gmail.com>
2022-09-19 21:41:57 +0900

    tab bar icons added

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import AppLoading from 'expo-app-loading';
-import React, { useState } from 'react';
-import { Text, Image } from 'react-native';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import { Asset, useAssets } from 'expo-asset';
-import { NavigationContainer } from '@react-navigation/native';
-import Tabs from './navigation/Tabs';
+import AppLoading from "expo-app-loading";
+import React, { useState } from "react";
+import { Text, Image } from "react-native";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset, useAssets } from "expo-asset";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
 
 const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
 const loadImages = (images) =>
   images.map((image) => {
-    if (typeof image === 'string') {
+    if (typeof image === "string") {
       return Image.prefetch(image);
     } else {
       return Asset.loadAsync(image);
@@ -19,11 +19,21 @@ const loadImages = (images) =>
   });
 
 export default function App() {
-  const [assets] = useAssets([require('./thumbs-up.jpeg')]);
-  const [loaded] = Font.useFonts(Ionicons.font);
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    const fonts = loadFonts([Ionicons.font]);
+    await Promise.all([...fonts]);
+  };
 
-  if (!assets || !loaded) {
-    return <AppLoading />;
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
   }
   return (
     <NavigationContainer>
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -5,6 +5,7 @@ import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import colors from "../colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -15,13 +16,13 @@ const Tabs = () => {
     <Tab.Navigator
       initialRouteName="Movie"
       screenOptions={{
-        // tabBarLabelStyle: { backgroundColor: 'red',},
         // tabBarLabelPosition: 'beside-icon',
         tabBarStyle: { backgroundColor: isDark ? colors.dark : "white" },
         tabBarActiveTintColor: isDark ? colors.orange : colors.dark,
         tabBarInactiveTintColor: isDark ? colors.gray_0 : colors.gray_1,
         headerStyle: { backgroundColor: isDark ? colors.dark : "white" },
         headerTitleStyle: { color: isDark ? "white" : colors.dark },
+        tabBarLabelStyle: { fontSize: 12, fontWeight: "600", marginTop: -6 },
       }}
     >
       <Tab.Screen
@@ -33,10 +34,34 @@ const Tabs = () => {
               <Text>Hello</Text>
             </View>
           ),
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="film-outline" color={color} size={size} />
+          ),
         }}
       />
-      <Tab.Screen name="Tv" component={Tv} options={{ tabBarBadge: 5 }} />
-      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarBadge: 5,
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="tv-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "search" : "search-outline"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
