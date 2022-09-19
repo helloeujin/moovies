d54f38ed16c6e4b30b5002c057c2461faa68cd99
Youjin <meggonagul@gmail.com>
2022-09-19 22:31:46 +0900

    stack navigator

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
+import Stack from "./navigation/Stack";
 
 const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
@@ -37,7 +38,7 @@ export default function App() {
   }
   return (
     <NavigationContainer>
-      <Tabs />
+      <Stack />
     </NavigationContainer>
   );
 }
---
+++ navigation/Stack.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, TouchableOpacity } from "react-native";
+import colors from "../colors";
+
+const ScreenOne = ({ navigation: { navigate } }) => (
+  <TouchableOpacity onPress={() => navigate("Two")}>
+    <Text>go to two</Text>
+  </TouchableOpacity>
+);
+const ScreenTwo = ({ navigation: { navigate } }) => (
+  <TouchableOpacity onPress={() => navigate("Three")}>
+    <Text>go to three</Text>
+  </TouchableOpacity>
+);
+const ScreenThree = ({ navigation: { setOptions } }) => (
+  <TouchableOpacity onPress={() => setOptions({ title: "Hello!" })}>
+    <Text>Change title</Text>
+  </TouchableOpacity>
+);
+
+const NativeStack = createNativeStackNavigator();
+
+const Stack = () => (
+  <NativeStack.Navigator
+    screenOptions={{
+      //   presentation: "modal",
+      headerBackTitleVisible: false,
+      headerShown: true,
+      headerTintColor: colors.orange,
+    }}
+  >
+    <NativeStack.Screen name="One" component={ScreenOne} />
+    <NativeStack.Screen name="Two" component={ScreenTwo} />
+    <NativeStack.Screen
+      name="Three"
+      component={ScreenThree}
+      options={{ presentation: "modal" }}
+    />
+  </NativeStack.Navigator>
+);
+
+export default Stack;
