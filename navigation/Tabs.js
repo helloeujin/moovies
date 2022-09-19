d4f80f6f50b00d48f863d4c166316f09cc587c4b
Youjin <meggonagul@gmail.com>
2022-09-19 17:25:57 +0900

    tab navigation options

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Movies from '../screens/Movies';
 import Tv from '../screens/Tv';
@@ -7,9 +8,32 @@ import Search from '../screens/Search';
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen name="Tv" component={Tv} />
+  <Tab.Navigator
+    initialRouteName="Movie"
+    screenOptions={{
+      tabBarLabelStyle: {
+        // backgroundColor: 'red',
+      },
+      //   tabBarLabelPosition: 'beside-icon',
+      //   tabBarActiveTintColor: '#6200ee',
+      tabBarStyle: {
+        // backgroundColor: 'yellow',
+      },
+    }}
+  >
+    <Tab.Screen
+      name="Movies"
+      component={Movies}
+      options={{
+        // headerTitleStyle: { color: 'tomato' },
+        headerRight: () => (
+          <View>
+            <Text>Hello</Text>
+          </View>
+        ),
+      }}
+    />
+    <Tab.Screen name="Tv" component={Tv} options={{ tabBarBadge: 5 }} />
     <Tab.Screen name="Search" component={Search} />
   </Tab.Navigator>
 );
