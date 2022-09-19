ff9dec132d7b68bc8dc5daf77e877b0be0287663
Youjin <meggonagul@gmail.com>
2022-09-19 20:56:14 +0900

    dark mode using useColorScheme

---
+++ colors.js
@@ -0,0 +1,8 @@
+const colors = {
+  dark: "#1e272e",
+  orange: "#ffa801",
+  gray_0: "#d2dae2",
+  gray_1: "#808e9b",
+};
+
+export default colors;
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,41 +1,44 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Movies from '../screens/Movies';
-import Tv from '../screens/Tv';
-import Search from '../screens/Search';
+import React, { useCallback } from "react";
+import { View, Text, useColorScheme } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Movies from "../screens/Movies";
+import Tv from "../screens/Tv";
+import Search from "../screens/Search";
+import colors from "../colors";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator
-    initialRouteName="Movie"
-    screenOptions={{
-      tabBarLabelStyle: {
-        // backgroundColor: 'red',
-      },
-      //   tabBarLabelPosition: 'beside-icon',
-      //   tabBarActiveTintColor: '#6200ee',
-      tabBarStyle: {
-        // backgroundColor: 'yellow',
-      },
-    }}
-  >
-    <Tab.Screen
-      name="Movies"
-      component={Movies}
-      options={{
-        // headerTitleStyle: { color: 'tomato' },
-        headerRight: () => (
-          <View>
-            <Text>Hello</Text>
-          </View>
-        ),
+const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+
+  return (
+    <Tab.Navigator
+      initialRouteName="Movie"
+      screenOptions={{
+        // tabBarLabelStyle: { backgroundColor: 'red',},
+        // tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: { backgroundColor: isDark ? colors.dark : "white" },
+        tabBarActiveTintColor: isDark ? colors.orange : colors.dark,
+        tabBarInactiveTintColor: isDark ? colors.gray_0 : colors.gray_1,
+        headerStyle: { backgroundColor: isDark ? colors.dark : "white" },
+        headerTitleStyle: { color: isDark ? "white" : colors.dark },
       }}
-    />
-    <Tab.Screen name="Tv" component={Tv} options={{ tabBarBadge: 5 }} />
-    <Tab.Screen name="Search" component={Search} />
-  </Tab.Navigator>
-);
+    >
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          headerRight: () => (
+            <View>
+              <Text>Hello</Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen name="Tv" component={Tv} options={{ tabBarBadge: 5 }} />
+      <Tab.Screen name="Search" component={Search} />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
