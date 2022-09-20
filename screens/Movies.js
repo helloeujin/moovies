0df8912aadee91e0352f2bdaa87333c64c07825b
Youjin <meggonagul@gmail.com>
2022-09-20 10:10:54 +0900

    styled components added

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { TouchableOpacity, Text } from "react-native";
+// import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import styled from "styled-components/native";
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: ${(props) => (props.selected ? "blue" : "red")};
+`;
 
 //// moving from Tabs to Stack navigator
 const Movies = ({ navigation: { navigate } }) => (
-  <TouchableOpacity
-    onPress={() => navigate("Stack", { screen: "One" })}
-    style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-  >
-    <Text>Movies</Text>
-  </TouchableOpacity>
+  <Btn onPress={() => navigate("Stack", { screen: "Two" })}>
+    <Title selected={false}>Movies</Title>
+  </Btn>
 );
 
 export default Movies;
