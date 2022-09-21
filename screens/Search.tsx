79e6dcb8ccd5f0579eb2807a6a173d1ebf9443f8
Youjin <meggonagul@gmail.com>
2022-09-21 20:49:11 +0900

    Swiper added, Movie API added

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text } from "react-native" ;
 
 const Search = () => (
   <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
