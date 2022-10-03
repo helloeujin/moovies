859df1e49a17d4880668e9c8ef0c6dae1bf173ca
Youjin <meggonagul@gmail.com>
2022-10-03 19:26:21 +0900

    make components folder for smaller components

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -11,59 +11,16 @@ import {
 import Swiper from "react-native-swiper";
 import styled from "styled-components/native";
 import { makeImgPath } from "../utilities";
-// import { BlurView } from "expo-blur";
+// import { BlurView } from "expo-blur"; // not working
 import { BlurView } from "@react-native-community/blur";
+import Slide from "../components/Slide";
 
 const Container = styled.ScrollView``;
-
-const View = styled.View`
-  flex: 1;
-`;
 
 const Loader = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-`;
-
-const BgImg = styled.Image`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-`;
-
-const Poster = styled.Image`
-  width: 100px;
-  height: 160px;
-  border-radius: 5px;
-`;
-
-const Title = styled.Text`
-  font-size: 16px;
-  font-weight: 600;
-  color: white;
-`;
-
-const Wrapper = styled.View`
-  flex-direction: row;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Column = styled.View`
-  width: 40%;
-  margin-left: 15px;
-`;
-
-const Overview = styled.Text`
-  margin-top: 10px;
-  color: rgba(255, 255, 255, 0.6);
-`;
-
-const Votes = styled(Overview)`
-  margin-top: 5px;
-  font-size: 12px;
 `;
 
 const API_KEY = "5e53d06765d63c2ad8fe05935854b26d";
@@ -72,9 +29,29 @@ const API_KEY = "5e53d06765d63c2ad8fe05935854b26d";
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
-  const isDark = useColorScheme() === "dark";
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [trending, setTrending] = useState([]);
+
+  const getTrending = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+    );
+    const { results } = await response.json();
+
+    setTrending(results);
+  };
+
+  const getUpcoming = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+    );
+    const { results } = await response.json();
+
+    setUpcoming(results);
+  };
+
   const getNowPlaying = async () => {
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
@@ -82,15 +59,16 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
     const { results } = await response.json();
 
     setNowPlaying(results);
+    // setLoading(false);
+  };
+
+  const getData = async () => {
+    await Promise.all([getTrending(), getUpcoming(), getNowPlaying()]);
     setLoading(false);
   };
 
   useEffect(() => {
-    getNowPlaying();
-    console.log({
-      name: "yj",
-      purpose: "test",
-    });
+    getData();
   }, []);
 
   return loading ? (
@@ -109,25 +87,14 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
         {nowPlaying.map((movie) => (
-          <View key={movie.id}>
-            <BgImg source={{ uri: makeImgPath(movie.backdrop_path) }} />
-            <BlurView
-              blurType={isDark ? "dark" : "light"}
-              blurAmount={10}
-              style={StyleSheet.absoluteFill}
-            >
-              <Wrapper>
-                <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
-                <Column>
-                  <Title>{movie.original_title}</Title>
-                  <Overview>{movie.overview.slice(0, 90)}...</Overview>
-                  {movie.vote_average > 0 ? (
-                    <Votes>⭐️{movie.vote_average}/10</Votes>
-                  ) : null}
-                </Column>
-              </Wrapper>
-            </BlurView>
-          </View>
+          <Slide
+            key={movie.id}
+            backdropPath={movie.backdrop_path}
+            posterPath={movie.poster_path}
+            originalTitle={movie.original_title}
+            voteAverage={movie.vote_average}
+            overview={movie.overview}
+          />
         ))}
       </Swiper>
     </Container>
--- themeStyled.js
+++ themeStyled.js
@@ -3,9 +3,11 @@
 export const lightTheme = {
   mainBgColor: "white",
   textColor: "#1e272e",
+  bodyTextColor: "rgba(0,0,0,0.85)",
 };
 
 export const darkTheme = {
   mainBgColor: "#1e272e",
   textColor: "#d2dae2",
+  bodyTextColor: "rgba(255,255,255,0.6)",
 };
