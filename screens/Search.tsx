8adf02104d64c8173ccdbfc9580561c327a26577
Youjin <meggonagul@gmail.com>
2022-10-11 20:48:08 +0900

    search api using query working

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,10 +1,62 @@
-import React from "react";
-import { View, Text } from "react-native" ;
+import React, { useState } from "react";
+import { Alert } from "react-native";
+import { useQuery } from "react-query";
+import styled from "styled-components";
+import { moviesAPI, tvAPI } from "../api";
 
-const Search = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>Search</Text>
-  </View>
-);
+const Container = styled.ScrollView``;
+
+const SearchBar = styled.TextInput`
+  background: white;
+  padding: 10px 15px;
+  border-radius: 15px;
+  width: 90%;
+  margin: 10px auto;
+`;
+
+const Search = () => {
+  const [query, setQuery] = useState("");
+
+  // refetch
+  const {
+    isLoading: moviesLoading,
+    data: moviesData,
+    refetch: searchMovies,
+  } = useQuery(["searchMovies", query], moviesAPI.search, {
+    enabled: false, // loading disabled
+  });
+
+  const {
+    isLoading: tvLoading,
+    data: tvData,
+    refetch: searchTv,
+  } = useQuery(["searchTv", query], tvAPI.search, {
+    enabled: false, // loading disabled
+  });
+
+  const onChangeText = (text: string) => {
+    setQuery(text);
+  };
+
+  const onSubmit = () => {
+    if (query === "") {
+      return;
+    }
+    searchMovies();
+    searchTv();
+  };
+
+  return (
+    <Container>
+      <SearchBar
+        placeholder="Search for Movie or TV Show"
+        placeholderTextColor="grey"
+        returnKeyType="search"
+        onChangeText={onChangeText}
+        onSubmitEditing={onSubmit}
+      />
+    </Container>
+  );
+};
 
 export default Search;
